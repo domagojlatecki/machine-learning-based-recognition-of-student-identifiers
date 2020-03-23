98e648e408cacab8fb579d431f6b06c144a61579
Domagoj Latečki <domagoj.latecki@gmail.com>
2020-03-23 20:03:57 +0100

    add debug output for train data

--- modules/preprocessing/main/scala/at/doml/thesis/Preprocessor.scala
+++ modules/preprocessing/main/scala/at/doml/thesis/Preprocessor.scala
@@ -1,21 +1,36 @@
 package at.doml.thesis
 
-import java.nio.file.Path
+import java.nio.file.{Path, Paths}
 import scala.util.{Failure, Success, Try}
 
 object Preprocessor {
 
-  def grayscale(c: RgbColor): RgbColor = {
+  private def grayscale(c: RgbColor): RgbColor = {
     val v = (Math.pow((c.r + c.g + c.b) / 768.0, 2.0) * 256.0).toInt
     RgbColor(v & 0xFF, v & 0xFF, v & 0xFF)
   }
 
-  def intensity(c: RgbColor): Int = c.r + c.g + c.b
+  private def intensity(c: RgbColor): Int = c.r + c.g + c.b
 
-  def apply(path: Path): Try[Canvas] = {
+  private final case class DebugData(fileName: Path, debugRoot: Path)
+
+  implicit final class CanvasOps(val c: Canvas) extends AnyVal {
+
+    def debugWrite(stepName: String)(implicit dd: Option[DebugData]): Canvas = {
+      dd.foreach { case DebugData(fileName, debugRoot) =>
+        println(s"[DEBUG] writing [$fileName] - $stepName")
+        c.writeTo(debugRoot.resolve(s"debug-$stepName").resolve(fileName))
+      }
+      c
+    }
+  }
+
+  def apply(path: Path, debugRoot: Option[Path] = None): Try[Canvas] = {
+    implicit val debugData: Option[DebugData] = debugRoot.map(DebugData(path.getFileName, _))
+
     val grayscaled = Canvas.fromPath(path)
       .map(grayscale)
-
+      .debugWrite("grayscale")
     val intensities = grayscaled.linear.map(intensity)
     val minIntensity = intensities.min
     val maxIntensity = intensities.max
@@ -23,7 +38,7 @@ object Preprocessor {
 
     val contrasted = grayscaled.map { c =>
       if (intensity(c) > cutoffPoint) RgbColor.White else RgbColor.Black
-    }
+    }.debugWrite("contrast")
 
     var pixelized = contrasted
 
@@ -38,10 +53,18 @@ object Preprocessor {
       }
     }
 
+    pixelized.debugWrite("pixelize")
+
     if (pixelized.linear.count(_ == RgbColor.Black) < 50) {
       Failure(new RuntimeException("Too low entropy image"))
     } else {
       Success(pixelized)
+    }
+  }
+
+  def main(args: Array[String]): Unit = {
+    Paths.get("data/input").toFile.listFiles.foreach{ f =>
+      apply(f.toPath, debugRoot = Some(Paths.get("data/")))
     }
   }
 }
